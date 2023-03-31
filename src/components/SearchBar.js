26056efe9cbf8ba2825bebf9ec74c4cbbeff002e
afbelardi <afbelardi@gmail.com>
2023-03-31 11:41:33 -0400

    added usestate for if user doesn't type a colleciton in the search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { useState } from 'react';
 import { Oval } from 'react-loading-icons';
 
 export default function SearchBar(props) {
 
+    const [badEntry, setBadEntry] = useState(false);
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -14,6 +17,9 @@ export default function SearchBar(props) {
           props.setSearchedCollection(response.data);
           props.setWasSearched(true)
           props.setLoading(false)
+          if (response.data === 'No Collection Provided') {
+            setBadEntry(true) 
+          }
         } catch (error) {
             console.error(error)
         }
@@ -25,10 +31,13 @@ export default function SearchBar(props) {
     return (
         <>
             <div className="flex items-center justify-center h-1/4">
-                <input className="w-2/5 h-12 text-center placeholder-gray-500 placeholder-opacity-50 rounded-2xl" placeholder="Cryptoon Goonz" type="text" ref={props.searchRef}></input>
+                <input className={`w-2/5 h-12 text-center placeholder-gray-500 placeholder-opacity-50 rounded-2xl ${badEntry ? 'bg-red-300 opacity-30' : ''}`} placeholder="Cryptoon Goonz" type="text" ref={props.searchRef}></input>
                 {
                    props.loading ? 
-                   < Oval /> :
+                   <Oval 
+                   stroke="#98ff98"
+                   className="ml-3"
+                   /> :
                    <button className="w-20 h-10 ml-4 rounded-md bg-slate-500" onClick={(e) => {handleSubmit(e); changeLoading()}}>TEST</button> 
                 }
                 
