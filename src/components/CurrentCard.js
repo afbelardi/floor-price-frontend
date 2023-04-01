5a0cac34f6fd174f35ebc93e65f80dcd04f9ff59
afbelardi <afbelardi@gmail.com>
2023-04-01 13:57:00 -0400

    fixed state to only show up with collection card if response status 200

--- src/components/CurrentCard.js
+++ src/components/CurrentCard.js
@@ -1,9 +1,14 @@
 import { useState } from "react"
 
 export default function CurrentCard (props) {
+
     return( 
         <>
-            
+        <div className="flex justify-center">
+            <div className="w-1/2 h-20 rounded-lg bg-slate-500">
+                <h1>{props.searchedCollection.collectionName}</h1>
+            </div>
+        </div>
         </>
 
     )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,9 +13,12 @@ export default function SearchBar(props) {
                 searchTerm: props.searchRef.current.value
             }
           );
+          if (response.status === 200) {
+            props.setWasSearched(true)
+          }
           console.log(response.data)
           props.setSearchedCollection(response.data);
-          props.setWasSearched(true)
+          
           props.setLoading(false)
           if (response.data === 'No Collection Provided') {
             setBadEntry(true) 
@@ -24,21 +27,22 @@ export default function SearchBar(props) {
             console.error(error)
         }
     }
+    
     const changeLoading = () => {
         props.setLoading(true)
     }
     
     return (
         <>
-            <div className="flex items-center justify-center h-1/4">
-                <input className={`w-2/5 h-12 text-center placeholder-gray-500 placeholder-opacity-50 rounded-2xl ${badEntry ? 'bg-red-300 opacity-30' : ''}`} placeholder="Cryptoon Goonz" type="text" ref={props.searchRef}></input>
+            <div className="flex items-center justify-center h-1/6">
+                <input className={`w-2/5 h-12 text-center placeholder-gray-500 placeholder-opacity-50 rounded-2xl ${badEntry ? 'border-red-500 bg-red-400 opacity-30 border-2' : ''}`} placeholder="Cryptoon Goonz" type="text" ref={props.searchRef}></input>
                 {
                    props.loading ? 
                    <Oval 
                    stroke="#98ff98"
                    className="ml-3"
                    /> :
-                   <button className="w-20 h-10 ml-4 rounded-md bg-slate-500" onClick={(e) => {handleSubmit(e); changeLoading()}}>TEST</button> 
+                   <button className="w-20 h-10 ml-4 rounded-md bg-slate-500" onClick={(e) => {handleSubmit(e); changeLoading()}}>Search</button> 
                 }
                 
             </div>
