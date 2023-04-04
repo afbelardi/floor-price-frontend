8305488ec994f4b76e3f78b082bd7b0474c50f2f
afbelardi <afbelardi@gmail.com>
2023-04-04 11:22:07 -0400

    minor layout changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@
 export default function Header() {
     return(
         <div className="flex items-center justify-center w-full bg-black h-10% min-w-300">
-            <h1 className="text-white text-7xl font-gilroy">Floor Price</h1>
+            <h1 className="pt-10 text-5xl text-white font-gilroy">Floor Price</h1>
         </div>
     )
 }
--- src/components/Watched.js
+++ src/components/Watched.js
@@ -4,15 +4,15 @@ export default function Watched(props) {
 
     const emptyWatched = (
         <>
-            <h1 className="mt-16 text-4xl text-white font-gilroy">Nothing to see here....yet</h1>
-            <span className="text-7xl">👀</span>
+            <h1 className="mt-16 text-4xl text-center text-white break-words font-gilroy">Nothing to see here....yet</h1>
+            <span className="mt-5 text-7xl">👀</span>
         </>
     )
 
     return (
         <>
-            <div className="flex flex-col items-center w-full mt-10 overflow-auto h-1/2">
-                <h1 className="p-6 text-6xl text-white font-gilroy">Watched</h1>
+            <div className="flex flex-col items-center w-full mt-10 h-1/2">
+                <h1 className="p-6 mt-10 text-6xl text-white font-gilroy">Watched</h1>
                 {
                     props.watchedNFTs.length === 0 ?
                     emptyWatched
